bc9f65e12887c2a7bcdc53d58ace97ba2092491d
leclm <lelima039@gmail.com>
2021-03-16 09:39:17 -0300

    Resolução do exercício 16 da lista 02

--- list02/ex16/expressoesBinarias.c
+++ list02/ex16/expressoesBinarias.c
@@ -8,19 +8,19 @@ e) O valor decimal e em hexadecimal de 0xFF e o resultado da operação 0xFF << 
 
 #include <stdio.h> // biblioteca com funções de input e output
 
-int main() {
+int main(void) {
     int r1 = ~0x55;
-    printf("a) 82 0x55 %x\n", r1);
+    printf("a) %x\n", r1);
 
     int r2 = 0x09 << 2;
-    printf("b) 9 0x09 %x\n", r2);
+    printf("b) %x\n", r2);
     
     int r3 = 0x09 >> 2;
-    printf("c) 9 0x09 %x\n", r3);
+    printf("c) %x\n", r3);
     
     int r4 = 0xFF >> 2;
-    printf("d) 255 0xFF %x\n", r4);
+    printf("d) %x\n", r4);
     
     int r5 = 0xFF << 2;
-    printf("e) 255 0xFF %x\n", r5);
+    printf("e) %x\n", r5);
 }
